e13ed8396af2854b53e6a87ef01f7ccc680c31f6
edgeless634 <edgeless634@protonmail.com>
2020-04-05 23:54:21 +0800

    改进画线逻辑，绘画速度提升近一倍

--- main.py
+++ main.py
@@ -4,8 +4,26 @@ import turtle
 import math
 
 # 调整画笔
-turtle.speed(2000)
+turtle.speed(3)
 turtle.pensize(3)
+
+class MyPen:
+    def __init__(self):
+        self.x, self.y = 0, 0
+    def goto(self, x, y):
+        if (self.x, self.y) == (x, y):
+            return
+        turtle.penup()
+        turtle.goto(x, y)
+        turtle.pendown()
+        self.x, self.y = x, y
+    def draw(self, x, y):
+        if (self.x, self.y) == (x, y):
+            return
+        turtle.goto(x, y)
+        self.x, self.y = x, y
+
+pen = MyPen()
 
 class dot:
     __slots__ = ("x", "y", "z")
@@ -13,8 +31,8 @@ class dot:
         self.x = x
         self.y = y
         self.z = z
-    def __eq__(self, dot):
-        return self.x == dot.x and self.y == dot.y and self.z == dot.z
+    def __eq__(self, obj):
+        return self.x == obj.x and self.y == obj.y and self.z == obj.z
     def __hash__(self):
         return self.x + self.y + self.z
     def toTwo(self):
@@ -26,33 +44,36 @@ class dot:
         '''
         点一个点
         '''
-        turtle.penup()
-        x, y = self.toTwo()
-        turtle.goto(x, y)
-        turtle.pendown()
+        px, py = self.toTwo()
+        
         turtle.circle(1, 360)
+        is_pen_down == True
 
 class line:
-    __slots__ = ("dot1", "dot2")
+    __slots__ = ("dot1", "dot2", "len")
     def __init__(self, dot1, dot2):
         self.dot1 = dot1
         self.dot2 = dot2
+        self.len = math.sqrt((self.dot1.x-self.dot2.x)**2 +\
+                            (self.dot1.y-self.dot2.y)**2 +\
+                            (self.dot1.z-self.dot2.z)**2)
     def __eq__(self, obj):
         return self.dot1 == obj.dot1 and self.dot2 == obj.dot2
     def __hash__(self):
         return self.dot1.__hash__() + self.dot2.__hash__()
+    def __lt__(self,obj):
+        if self.len != obj.len:
+            return self.len < obj.len
+        else:
+            return self.dot1.x + self.dot1.y + self.dot1.z < obj.dot1.x + obj.dot1.y + obj.dot1.z
     def draw(self):
         '''
         画出一条线
         '''
-        turtle.penup()
         x, y = self.dot1.toTwo()
-        turtle.goto(x, y)
-        turtle.pendown()
+        pen.goto(x, y)
         x, y = self.dot2.toTwo()
-        turtle.goto(x, y)
-    def len(self):
-        return math.sqrt((self.dot1.x-self.dot2.x)**2 + (self.dot1.y-self.dot2.y)**2 + (self.dot1.z-self.dot2.z)**2)
+        pen.draw(x, y)
 
 class cube:
     __slots__ = ("A", "B", "C", "D", "A1", "B1", "C1", "D1", "lines")
@@ -327,7 +348,7 @@ def pixelsToCube(pixels, dotLD, length):
                 cubes.append(cube(dot1, dot2))
     for i in range(1, len(cubes)-1):
         for j in range(i+2, len(cubes)):
-            if line(cubes[i].A, cubes[j].A).len() < line(cubes[i].A, cubes[i+1].A).len():
+            if line(cubes[i].A, cubes[j].A).len < line(cubes[i].A, cubes[i+1].A).len:
                 cubes[j], cubes[i+1] = cubes[i+1], cubes[j]
     return cubes
 
@@ -352,19 +373,34 @@ def numsToCube(n:str, *, length = 20, dotLD = None, offset = dot(0, 0, 0)):
         dotLD = dot(dotLD.x + length * (len(pix[0])+1), dotLD.y, dotLD.z)
     return cubes
 
-def drawCubes(cubes:list):
+def drawCubes(cubes: list):
+    '''
     lines = set()
     for cu in cubes:
         for line in cu.lines:
             lines.add(line)
+    for line in sorted(list(lines)):
+        line.draw()
+    '''
+    lines = []
+    for cu in cubes:
+        for line in cu.lines:
+            if line in lines:
+                continue
+            for i in range(len(lines)-1):
+                if line.dot1 == lines[i].dot2 and lines[i+1].dot1 != lines[i].dot2:
+                    lines.insert(i+1, line)
+                    break
+            else:
+                lines.append(line)
     for line in lines:
         line.draw()
-    
+
 t1 = time.time()
-for cu in numsToCube("abc", length = 20):
-    cu.draw()
+drawCubes(numsToCube("abc", length=20, offset=dot(0, 0, -120)))
 print(time.time()-t1)
 t1 = time.time()
-drawCubes(numsToCube("abc", length = 20, offset=dot(0, 0, -120)))
+for cu in numsToCube("abc", length=20):
+    cu.draw()
 print(time.time()-t1)
 time.sleep(2)
